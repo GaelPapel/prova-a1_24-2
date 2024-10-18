8527a5672d483c8eeaec89d12c336494e50790a2
Gabriel SF <gaelsempapel@gmail.com>
2024-10-18 18:38:32 -0300

    Renomeia arquivo

--- src/Exercicio2/Exercicio2.java
+++ src/Exercicio2/Exercicio2.java
@@ -1,6 +1,6 @@
 package Exercicio2;
 import java.util.Scanner;
-public class java {
+public class Exercicio2 {
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
